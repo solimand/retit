2b49f1b7c1f4304b014c719ef28822c1f907cb71
misolimando <m.solimand@gmail.com>
2019-09-24 17:51:45 +0200

    Used strstr

--- RetiT-groups_cgi.c
+++ RetiT-groups_cgi.c
@@ -4,8 +4,6 @@
 
 //check if a student (string -matricola-) is inside a text file (containing all students)
 int checkStud (char * id, char* filename);
-// Returns 0 if s1 is substring of s2 
-int isSubstring(char *s1, char *s2);
 
 //Test-MAIN
 int main(int argc, char**argv) {
@@ -30,37 +28,9 @@ int checkStud (char * id, char* filename){
                 printf("file %s opened\n", filename);
         
         while (fscanf(file,"%s", string) == 1){
-                res = isSubstring(id, string);
-                if (res==0){
-                        fclose(file);
+                if(strstr(string, id)!=NULL)
                         return 1;
-                }
         }
         fclose(file);
         return -1;
 }
-
-int isSubstring(char *s1, char *s2){ 
-        int M = strlen(s1); //substring matricola =10
-        int N = strlen(s2); //main-string
-
-        char strTmp [M+1];
-        strTmp[0]='\0';
-
-        if(N>M){
-                // slider-loop
-                for (int i = 0; i <= N - M; i++) { 
-                        //substring from i to i+M
-                        strncpy(strTmp, s2+i, M);
-                        strTmp[M]='\0';
-
-                        //DBG
-                        //printf("tmp string %s \n", strTmp);
-
-                        if (strcmp(strTmp, s1)==0)
-                                return 0;
-                }
-        } 
-
-        return -1; 
-} 
